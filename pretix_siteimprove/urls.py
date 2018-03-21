0d227255af0d5e8702f7a496e09c0e249dda1b2a
Mikkel Ricky <rimi@aarhus.dk>
2018-03-21 10:31:26 +0100

    EBILL-2: Fixed url pattern

--- pretix_siteimprove/urls.py
+++ pretix_siteimprove/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import siteimprove
 
 urlpatterns = [
-    url(r'^_pretix_siteimprove/siteimprove$', siteimprove, name='siteimprove'),
+    url(r'^_pretix_siteimprove/siteimprove/$', siteimprove, name='siteimprove'),
 ]
