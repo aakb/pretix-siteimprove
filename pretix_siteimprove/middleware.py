bd21962a11cd1317b6c75e98eca266ed3ad6bd5c
Mikkel Ricky <rimi@aarhus.dk>
2018-04-10 14:57:42 +0200

    EBILL-20: Allowed inline styling from Siteimprove

--- pretix_siteimprove/middleware.py
+++ pretix_siteimprove/middleware.py
@@ -6,10 +6,12 @@ from pretix.base.middleware import (
 
 class SecurityMiddleware(BaseSecurityMiddleware):
     def process_response(self, request, resp):
-        # Whitelist siteimprove urls in CSP.
         h = {
+            # Whitelist siteimprove urls in CSP.
             'script-src': ['https://siteimproveanalytics.com', 'https://*.siteimprove.com'],
             'img-src': ['https://*.siteimprove.com'],
+            # Siteimprove adds inline styling.
+            'style-src': ['\'unsafe-inline\''],
         }
 
         # Copied from super().process_response
